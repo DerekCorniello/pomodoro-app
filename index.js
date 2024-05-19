0544247f77c10ace5fcee14370d573660809b349
DerekCorniello <dcorniello.business@gmail.com>
2024-05-18 23:27:55 -0400

    updated timer format, added testing framework

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let mainWindow;
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
+        // TODO Dynamic changes?
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true,
+            contextIsolation: false,
         },
     });
 
@@ -21,13 +23,15 @@ function createMainWindow() {
 }
 
 function switchToTimerWindow() {
-    mainWindow.loadFile("views/timer.html");
-    mainWindow.webContents.executeJavaScript("startPomodoro()");
+    mainWindow.loadFile("views/timer.html").then(() => {
+        mainWindow.webContents.executeJavaScript("window.doPomodoro();");
+    });
 }
 
 app.whenReady().then(() => {
     createMainWindow();
     // Register 'Enter' key shortcut to switch to timer window
+    // TODO: Create UI to handle this event
     globalShortcut.register("Enter", () => {
         console.log("Enter key pressed");
         switchToTimerWindow();
--- scripts/timer.js
+++ scripts/timer.js
@@ -1,3 +1,4 @@
+// Enum type for a stage
 const StageType = Object.freeze({
     WorkOne: 0,
     BreakOne: 1,
@@ -9,57 +10,84 @@ const StageType = Object.freeze({
     BreakFour: 7,
 });
 
+
+// class for representing a stage
+// arguments -> ptype is a StageType, duration is an number value for minutes
 class Stage {
     constructor(ptype, duration) {
         this.type = ptype;
         this.duration = duration;
     }
     toString() {
-        return String(`Stage ${this.type.toString()}, Duration ${this.duration.toString()}`);
+        return `Stage ${this.type.toString()}, Duration ${this.duration.toString()}`;
     }
 }
 
+// class for representing an entire pomdoro sequence
+// has private variable sequence for intuitive checking of inputs
+// inputs a list of 8 Stage types
+class PomodoroSequence {
+    #sequence;
 
-function doPomodoro(sequence) {
-    const stage = sequence.shift();
-    if (!stage) {
-        console.log("Sequence is empty. Pomodoro completed!");
-        return;
+    constructor(sequence = null) {
+        this.setSequence(sequence);
     }
 
-    const stageType = Object.keys(StageType).find(key => StageType[key] === stage.type);
-
-    console.log(`Starting ${stageType} for ${stage.duration} minutes.`);
-
-    setTimeout(() => {
-        console.log(`Completed ${stageType}.`);
-
-        if (sequence.length > 0) {
-            doPomodoro(sequence);
+    // set the sequence, check to ensure it is valid as well
+    setSequence(sequence) {
+        if (sequence === 8) {
+            this.#sequence = sequence;
         } else {
-            console.log("Pomodoro completed!");
+            // use the default pomodoro sequence
+            this.#sequence = [
+                new Stage(StageType.WorkOne, 25),
+                new Stage(StageType.BreakOne, 5),
+                new Stage(StageType.WorkTwo, 25),
+                new Stage(StageType.BreakTwo, 5),
+                new Stage(StageType.WorkThree, 25),
+                new Stage(StageType.BreakThree, 5),
+                new Stage(StageType.WorkFour, 10),
+                new Stage(StageType.BreakFour, 20),
+            ];
         }
-    }, stage.duration * 60000); // Convert minutes to milliseconds
-}
-
-function startPomodoro(sequence) {
-    console.log("pomodoro entered")
-    defPomodoroSequence = [
-        new Stage(StageType.WorkOne, 25),
-        new Stage(StageType.BreakOne, 5),
-        new Stage(StageType.WorkTwo, 25),
-        new Stage(StageType.BreakTwo, 5),
-        new Stage(StageType.WorkThree, 25),
-        new Stage(StageType.BreakThree, 5),
-        new Stage(StageType.WorkFour, 10),
-        new Stage(StageType.BreakFour, 20),
-    ];
-    sequence = defPomodoroSequence || sequence;
-    if (sequence.length !== 8) {
-        throw new Error("Error: Pomodoro Sequence is not 8 stages."); //TODO: Maybe this isn't a constraint
     }
-    console.log(sequence);
-    return doPomodoro(sequence);
+
+    // get the sequence list
+    getSequence() {
+        return this.#sequence;
+    }
+
+    // execute the sequence
+    execute() {
+        // create a copy of the sequence so data can be retained
+        const sequence = this.getSequence().slice();
+
+        // create a recursive func that will manage the pomodoro
+        const runStage = (seq) => {
+            // base case, if there are no more stages, end
+            if (seq.length === 0) {
+                console.log("Pomodoro completed!");
+                return;
+            }
+            // takes the first element, get a bunch of its info
+            const stage = seq.shift();
+            const stageType = Object.keys(StageType).find(key => StageType[key] === stage.type);
+
+            console.log(`Starting ${stageType} for ${stage.duration} minutes.`);
+
+            setTimeout(() => {
+                console.log(`Completed ${stageType}.`);
+                runStage(seq); // continue with the next stage
+            }, stage.duration * 60000); // Convert minutes to milliseconds
+
+            // TODO: Somehow await for a event before moving on?
+
+        };
+
+        runStage(sequence);
+    }
 }
 
-window.startPomodoro = startPomodoro;
+window.doPomodoro = () => {
+    new PomodoroSequence().execute();
+};
