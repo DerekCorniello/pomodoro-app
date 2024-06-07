4ab56f5d10d62399a199c2eb14367e53a57d6e42
DerekCorniello <dcorniello.business@gmail.com>
2024-06-07 18:35:35 -0400

    Added more ts, user info and such

--- scripts/todo.ts
+++ scripts/todo.ts
@@ -1,10 +1,29 @@
-class TodoList {
+import assert from 'assert';
+
+export class TodoList {
     title: string;
     items: ListItem[];
 
     constructor(title: string, items: ListItem[]) {
         this.title = title;
         this.items = items;
+    }
+
+    private deleteAt(index: number) {
+        assert(index >= 0 && index < this.items.length, "Index out of Range!");
+        return this.items.filter((_, i) => i !== index);
+    }
+
+    addItem(item: ListItem) {
+        this.items.push(item);
+    }
+
+    deleteItemAtIndex(index: number) {
+        this.deleteAt(index)
+    }
+
+    deleteItemAtName(name: string) {
+        this.deleteAt(this.items.findIndex(item => item.title === name));
     }
 }
 
