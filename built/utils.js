28b3c6d7593dc2ad39e8d74aa13cade795619881
DerekCorniello <dcorniello.business@gmail.com>
2024-06-13 00:27:49 -0400

    fixed stupid js/ts/electron issues

--- built/utils.js
+++ built/utils.js
@@ -1,84 +1,67 @@
+"use strict";
 const { assert } = require('assert');
-
 class TodoList {
-    title: string;
-    items: ListItem[];
-
-    constructor(title: string, items: ListItem[]) {
+    constructor(title, items) {
         this.title = title;
         this.items = items;
     }
-
-    private deleteAt(index: number) {
+    deleteAt(index) {
         assert(index >= 0 && index < this.items.length, "Index out of Range!");
         return this.items.filter((_, i) => i !== index);
     }
-
-    addItem(item: ListItem) {
+    addItem(item) {
         this.items.push(item);
     }
-
-    deleteItemAtIndex(index: number) {
-        this.deleteAt(index)
+    deleteItemAtIndex(index) {
+        this.deleteAt(index);
     }
-
-    deleteItemAtName(name: string) {
+    deleteItemAtName(name) {
         this.deleteAt(this.items.findIndex(item => item.title === name));
     }
 }
-
 class ListItem {
-    title: string;
-    dueDate: Date | null;
-    description: string | null;
-
-    constructor(title: string, dueDate: Date | null = null, description: string | null = null) {
+    constructor(title, dueDate = null, description = null) {
         this.title = title;
         this.dueDate = dueDate;
         this.description = description;
     }
 }
-enum StageType {
-    WorkOne,
-    BreakOne,
-    WorkTwo,
-    BreakTwo,
-    WorkThree,
-    BreakThree,
-    WorkFour,
-    BreakFour
-};
-
+var StageType;
+(function (StageType) {
+    StageType[StageType["WorkOne"] = 0] = "WorkOne";
+    StageType[StageType["BreakOne"] = 1] = "BreakOne";
+    StageType[StageType["WorkTwo"] = 2] = "WorkTwo";
+    StageType[StageType["BreakTwo"] = 3] = "BreakTwo";
+    StageType[StageType["WorkThree"] = 4] = "WorkThree";
+    StageType[StageType["BreakThree"] = 5] = "BreakThree";
+    StageType[StageType["WorkFour"] = 6] = "WorkFour";
+    StageType[StageType["BreakFour"] = 7] = "BreakFour";
+})(StageType || (StageType = {}));
+;
 // Class for representing a stage
 // ptype is a StageType, duration is a number value for minutes
 class Stage {
-    type: StageType;
-    duration: number;
-
-    constructor(ptype: StageType, duration: number) {
+    constructor(ptype, duration) {
         this.type = ptype;
         this.duration = duration;
     }
-
-    toString(): string {
+    toString() {
         return `Stage ${StageType[this.type]}, Duration ${this.duration.toString()}`;
     }
 }
-
 // Class for representing an entire pomodoro sequence
 // inputs a list of 8 Stage types
 class PomodoroSequence {
-    private sequence: Stage[] = [];
-
-    constructor(sequence: Stage[] | null = null) {
+    constructor(sequence = null) {
+        this.sequence = [];
         this.setSequence(sequence);
     }
-
     // Set the sequence, check to ensure it is valid as well
-    setSequence(sequence: Stage[] | null): void {
+    setSequence(sequence) {
         if (sequence && sequence.length === 8) {
             this.sequence = sequence;
-        } else {
+        }
+        else {
             // Use the default pomodoro sequence
             this.sequence = [
                 new Stage(StageType.WorkOne, 25),
@@ -92,56 +75,42 @@ class PomodoroSequence {
             ];
         }
     }
-
     // Get the sequence list
-    getSequence(): Stage[] {
+    getSequence() {
         return this.sequence;
     }
-
     // Execute the sequence
-    execute(): void {
+    execute() {
         // Create a copy of the sequence so data can be retained
         const sequence = this.getSequence().slice();
-
         // Create a recursive function that will manage the pomodoro
-        const runStage = (seq: Stage[]): void => {
+        const runStage = (seq) => {
             // Base case, if there are no more stages, end
             if (seq.length === 0) {
                 console.log("Pomodoro completed!");
                 return;
             }
-
             // Takes the first element, get a bunch of its info
-            const stage = seq.shift()!;
+            const stage = seq.shift();
             const stageType = StageType[stage.type];
-
             console.log(`Starting ${stageType} for ${stage.duration} minutes.`);
-
             setTimeout(() => {
                 console.log(`Completed ${stageType}.`);
                 runStage(seq); // Continue with the next stage
             }, stage.duration * 60000); // Convert minutes to milliseconds
         };
-
         runStage(sequence);
     }
 }
-
 window.doPomodoro = () => {
-    console.log("doPomodoro Created")
+    console.log("doPomodoro Created");
     new PomodoroSequence().execute();
 };
 // If we have a login and logout functionality,
 // I think this class is just a singleton at that
 // point, idk if we need to do anything special with it
 class User {
-    private username: string;
-    private password: string;
-    TodoList: TodoList[] | null;
-    history: TodoList[] | null;
-    customPomodoros: PomodoroSequence[] | null;
-
-    constructor(username: string, password: string, todo: TodoList[] | null = [], history: TodoList[] | null = null, customPomodoros: PomodoroSequence[] | null = null) {
+    constructor(username, password, todo = [], history = null, customPomodoros = null) {
         this.username = username;
         this.validateUsername();
         this.password = password;
@@ -150,24 +119,20 @@ class User {
         this.history = history;
         this.customPomodoros = customPomodoros;
     }
-
-    static login(user: User, username: string, password: string) {
+    static login(user, username, password) {
         // TODO
         // Connect to db or something idk
     }
-
-    static logout(user: User) {
+    static logout(user) {
         // TODO
         // Disconnect from db lol?
     }
-
     validateUsername() {
         // TODO
-        return this.username
+        return this.username;
     }
     validatePassword() {
         // TODO
-        return this.password
+        return this.password;
     }
-
 }
